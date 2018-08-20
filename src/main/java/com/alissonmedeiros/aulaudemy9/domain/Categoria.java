cd4f427c120f4c5d391d6642172b93f5a6590780
Alisson Medeiros <alisilva1@gmail.com>
2018-08-20 16:54:47 -0300

    Testando H2 e criacao de bancos funcionando

--- src/main/java/com/alissonmedeiros/aulaudemy9/domain/Categoria.java
+++ src/main/java/com/alissonmedeiros/aulaudemy9/domain/Categoria.java
@@ -2,9 +2,18 @@ package com.alissonmedeiros.aulaudemy9.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
 	//significa que ela é a versão 1
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
 	private String nome;
 	
