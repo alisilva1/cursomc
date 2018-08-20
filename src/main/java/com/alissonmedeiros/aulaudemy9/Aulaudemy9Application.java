5d85608fc15a3ee27f6a7fec8afb545a1f4adb53
Alisson Medeiros <alisilva1@gmail.com>
2018-08-20 15:03:22 -0300

    Projeto criado

---
+++ src/main/java/com/alissonmedeiros/aulaudemy9/Aulaudemy9Application.java
@@ -0,0 +1,12 @@
+package com.alissonmedeiros.aulaudemy9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aulaudemy9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aulaudemy9Application.class, args);
+	}
+}
